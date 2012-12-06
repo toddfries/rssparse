92a73ce09d3857d5e3abb620f74590e171f5be78
Todd T. Fries <todd@fries.net>
2012-12-06 12:05:56 -0600

    o implement wordwrap and tagline as options to new HTML::FormatText::WithImgLinks o handle pre/code sections properly, makes e.g. gedcom html documentation readable

--- FDC/CleanText.pm
+++ FDC/CleanText.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 Todd T. Fries <todd@fries.net>
+# Copyright (c) 2011,2012 Todd T. Fries <todd@fries.net>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
